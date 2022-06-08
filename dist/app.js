9a568eed39b405db8ad0cc622200d1b6ec134f88
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2022-06-08 17:18:22 +0100

    created the abstract component base clase

--- dist/app.js
+++ dist/app.js
@@ -84,6 +84,22 @@ function autoBind(target, methodName, descriptor) {
     };
     return adjDescription;
 }
+// Component Base Class
+class Component {
+    constructor(templateId, hostElementId, insertAtStart, newElementId) {
+        this.templateElement = document.getElementById(templateId);
+        this.hostElement = document.getElementById(hostElementId);
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        if (newElementId) {
+            this.element.id = newElementId;
+        }
+        this.attach(insertAtStart);
+    }
+    attach(insertAtBeginning) {
+        this.hostElement.insertAdjacentElement(insertAtBeginning ? "afterbegin" : "beforeend", this.element);
+    }
+}
 // Project List Class
 class ProjectList {
     constructor(type) {
